97d91c84737ddb1e587d00bf428314ae1331df4d
Sujeet Mainali <mainalisujeet@gmail.com>
2023-09-28 23:48:54 +0545

    CICD test

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -50,7 +50,7 @@ if (DotenvConfig.NODE_ENV === 'PRODUCTION') {
   const swaggerDocument = require('../../build/swagger.json');
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-  app.get('/', (req, res) => res.send('Please set to the production; CI CD is successful tho'));
+  app.get('/', (req, res) => res.send('Please set to the production; CI CD is successful tho finally with pm2 server automatically setup also'));
 }
 
 app.use(ErrorHandler);
